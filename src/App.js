6e51540a722cfb1f28a4f69047dff29c31ff6171
Amelie121212 <amel.1989ab@gmail.com>
2020-04-10 08:01:11 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import 'weather-icons/css/weather-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from'./app_component/weather.component';
+import Form from'./app_component/form.component';
+
+const API_key="45b6805fb3b828d085ed5b0edd5b9f6d";
 
 class App extends React.Component {
   constructor() {
@@ -18,8 +21,6 @@ class App extends React.Component {
       description: "",
       error: false
     };
-    this.getWeather();
-
     this.weathericon={
       Thunderstorm:"wi-thunderstorm",
       Drizzle: "wi-sleet",
@@ -61,23 +62,31 @@ class App extends React.Component {
         this.setState({icon:this.weathericon.Clouds});
     }
   }
-  getWeather = async() => {
-    const api_call= await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=45b6805fb3b828d085ed5b0edd5b9f6d');
-    const response= await api_call.json();
-    console.log(response);
-    this.setState({
-      city: response.name,
-      country: response.sys.country,
-      celsius: this.calCelsius(response.main.temp),
-      temp_max: this.calCelsius(response.main.temp_max),
-      temp_min: this.calCelsius(response.main.temp_min),
-      description: response.weather[0].description,
-    });
-    this.get_Weathericon(this.weathericon, response.weather[0].id);
+  getWeather = async(event) => {
+    event.preventDefault();
+    const country= event.target.elements.country.value;
+    const city= event.target.elements.city.value;
+    if(city && country){
+      const api_call= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
+      const response= await api_call.json();
+      console.log(response);
+      this.setState({
+        city: response.name,
+        country: response.sys.country,
+        celsius: this.calCelsius(response.main.temp),
+        temp_max: this.calCelsius(response.main.temp_max),
+        temp_min: this.calCelsius(response.main.temp_min),
+        description: response.weather[0].description,
+      });
+      this.get_Weathericon(this.weathericon, response.weather[0].id);
+    } else {
+      this.setState({error:true});
+    }
   };
   render() {
     return(
       <div className="App">
+        <Form loadweather={this.getWeather} error={this.state.error}/>
         <Weather 
           city={this.state.city} 
           country={this.state.country} 
--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Weather = (props) => {
     return(
-        <div className="container">
-            <div className="cards">
-                <h1>{props.city}, {props.country}</h1>
+        <div className="bd-highlight container">
+            <div>{props.error ? error():null}</div>
+            <div className="cards pt-4">
+                {props.city?(<h1>{props.city}, {props.country}</h1>):null}
                 <h5 className="py-4">
                     <i className={`wi ${props.weathericon} display-1`}></i>
                 </h5>
-                <h1 className="py-2">{props.temp_celsius}&deg;</h1>
-                {/* show max and min temp */}
+                {props.temp_celsius?(<h1 className="py-2">{props.temp_celsius}&deg;</h1>):null}
                 {minmaxTemp(props.temp_min,props.temp_max)}
                 <h4 className="py-4">{props.description}</h4>
             </div>
@@ -18,11 +18,20 @@ const Weather = (props) => {
 };
 
 function minmaxTemp(min,max) {
-    return (
-        <h3>
-            <span className="px-4">{min}&deg;</span>
-            <span className="px-4">{max}&deg;</span>
-        </h3>
+    if(min && max){
+        return (
+            <h3>
+                <span className="px-4">{min}&deg;</span>
+                <span className="px-4">{max}&deg;</span>
+            </h3>
+        );
+    }
+}
+function error(){
+    return(
+        <div className="alert alert-danger mx-5" role="alert">
+            Please Enter Country and City.
+        </div>
     );
 }
 
